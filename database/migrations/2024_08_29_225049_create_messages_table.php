f427110b90ad9cb3c5336abc305d136b7520cb15
arielchama <arielchama45@gmail.com>
2024-09-08 01:42:01 +0100

    feat: add migrates

---
+++ database/migrations/2024_08_29_225049_create_messages_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        if (!Schema::hasTable('messages')) {
+            Schema::create('messages', function (Blueprint $table) {
+                $table->id();
+                $table->foreignId('conversation_id')->constrained('conversations')->onDelete('cascade');
+                $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
+                $table->text('content');
+                $table->timestamps();
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('messages');
+    }
+};
